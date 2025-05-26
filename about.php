1e1c23033ea18ccab02b7565dee86a82d07c08ff
Nathalia010 <90879781+Nathalia010@users.noreply.github.com>
2025-05-25 23:06:13 -0500

    cambio de php a html 5

--- about.php
+++
@@ -1,2 +0,0 @@
-<h2>Sobre mí</h2>
-<p>Soy Técnica Profesional en Mantenimiento de Bases de Datos con formación en programación (JavaScript, PHP, Python) y experiencia en gestión de datos en MariaDB. Actualmente curso el sexto semestre de Ingeniería de Sistemas.</p>
--- contact.php
+++
@@ -1,7 +0,0 @@
-<h2>Contacto</h2>
-<table>
-  <tr><td>📧 Correo:</td><td><a href="mailto:k.nathalia.martinez.b@gmail.com">k.nathalia.martinez.b@gmail.com</a></td></tr>
-  <tr><td>📱 Teléfono:</td><td>+57 301 3258684</td></tr>
-  <tr><td>🌐 LinkedIn:</td><td><a href="https://www.linkedin.com/in/karen-nathalia-martinez-biscunda-ingsistemas" target="_blank">Perfil</a></td></tr>
-  <tr><td>📍 Ubicación:</td><td>Bogotá, Colombia</td></tr>
-</table>
